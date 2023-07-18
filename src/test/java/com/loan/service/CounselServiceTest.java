0867c3d35dcd38fdeaa586355928401ea48fe821
KSH <sh7824@naver.com>
2023-07-18 17:00:50 +0900

    #1 - 대출 조회 기능 Test
    상담 ID 가 존재하는 경우 , 존재하지않는 경우 테스트
    모두 통과

--- src/test/java/com/loan/service/CounselServiceTest.java
+++ src/test/java/com/loan/service/CounselServiceTest.java
@@ -1,7 +1,10 @@
 package com.loan.service;
 
 import com.loan.domain.Counsel;
+import com.loan.exception.BaseException;
+import com.loan.exception.ResultType;
 import com.loan.repository.CounselRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -13,6 +16,8 @@ import org.modelmapper.ModelMapper;
 import com.loan.dto.CounselDTO.Request;
 import com.loan.dto.CounselDTO.Response;
 
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -57,8 +62,34 @@ public class CounselServiceTest { // 유닛 테스트
         Response actual = counselService.create(request);
 
         assertThat(actual.getName()).isSameAs(entity.getName());
-
     }
 
+
+    @Test
+    void Should_ReturnResponseOfExistCounselEntity_When_RequestExistCounselId(){ // 대출 상담 ID 가 존재하는 경우에만 리스폰스를 리턴
+
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = counselService.get(findId);
+
+        assertThat(actual.getCounselId()).isSameAs(findId);
+    }
+
+    @Test
+    void Should_ThrowException_When_RequestNotExistCounselId(){ // 존재 하지 않는 ID 로 조회시 익셉션을 던져준다.
+        Long findId = 2L;
+
+        when(counselRepository.findById(findId)).thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        Assertions.assertThrows(BaseException.class,()-> counselService.get(findId));
+
+
+    }
 
     }
