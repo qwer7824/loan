b08f62983f874ba6c2e9aba7ffcf5b6c949276c2
KSH <sh7824@naver.com>
2023-07-20 16:50:17 +0900

    #5 - 상담 삭제 기능 Test
    성공

--- src/test/java/com/loan/service/CounselServiceTest.java
+++ src/test/java/com/loan/service/CounselServiceTest.java
@@ -114,4 +114,21 @@ public class CounselServiceTest { // 유닛 테스트
         assertThat(actual.getCounselId()).isSameAs(findId);
         assertThat(actual.getName()).isSameAs(request.getName());
     }
+
+    @Test
+    void Should_DeletedCounselEntity_When_RequestDeleteExistCounselInfo(){
+         Long targetId = 1L;
+
+         Counsel entity = Counsel.builder()
+                 .counselId(1L)
+                 .build();
+
+        when(counselRepository.save(ArgumentMatchers.any(Counsel.class))).thenReturn(entity);
+        when(counselRepository.findById(targetId)).thenReturn(Optional.ofNullable(entity));
+
+
+        counselService.delete(targetId);
+
+        assertThat(entity.getIsDeleted()).isSameAs(true);
+    }
     }
